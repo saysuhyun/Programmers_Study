06c5dc4d007e0c0f0a2b7debc4c38170c989c9ae
unknown <ibmitw@naver.com>
2024-09-04 21:35:49 +0900

    feat: 두 정수

--- sanghyun/1week/SameNumber.java
+++ sanghyun/1week/SameNumber.java
@@ -10,6 +10,7 @@ public class SameNumber {
     public static ArrayList<Integer> solve(int[] arr) {
         ArrayList<Integer> answerList = new ArrayList<>();
         for (int i = 0; i < arr.length; i++) {
+            // list가 비어있거나 가장 마지막 배열 값이랑 현재 값이 다른경우에만 add
             if (answerList.isEmpty() || answerList.get(answerList.size() - 1) != arr[i]) {
                 answerList.add(arr[i]);
             }
