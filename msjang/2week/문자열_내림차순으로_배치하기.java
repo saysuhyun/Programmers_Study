315b78da7cfe08d9e3bf1d8f55ae169143055d47
min-gui <msj349911@gmail.com>
2024-09-12 00:53:12 +0900

    fix: 문자열 내림자순으로 배치하기

--- msjang/2week/문자열_내림차순으로_배치하기.java
+++ msjang/2week/문자열_내림차순으로_배치하기.java
@@ -11,10 +11,15 @@ public class 문자열_내림차순으로_배치하기 {
 
     static class Solution {
         public String solution(String s) {
-            char[] c = s.toCharArray();
-            Arrays.sort(c);
-            String str = new String(c);
-            return new StringBuilder(str).reverse().toString();
+              // StringBuilder 를 이용하는 방법 보다 느리다.
+//            String[] split = s.split("");
+//            Arrays.sort(split, Collections.reverseOrder());
+//            return String.join("", split);
+
+
+            char[] charArray= s.toCharArray();
+            Arrays.sort(charArray);
+            return new StringBuilder(new String(charArray)).reverse().toString();
         }
     }
 }
