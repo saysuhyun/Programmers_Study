eb34adb5b33a93336c1d21882ff4dad46c7d7495
saysuhyun <saysuhyun@gmail.com>
2024-09-14 00:01:17 +0900

    feat: 문자열 내 p와 y의 개수

---
+++ suhyun/2week/문자열_내_p와_y의_개수.java
@@ -0,0 +1,25 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class 문자열_내_p와_y의_개수 {
+    // 문자열 s가 주어지는데, 대문자와 소문자가 섞여있다.
+    // 타켓은 p와 y
+    // return : p length == y length
+    // if(p length, y length == 0) return true
+
+    // 방법
+    // 1. 하나의 문자열에서 특정 문자의 갯수 구하기(함수화) : .filter(s -> s == ch).count();
+    // 2. 대문자와 소문자를 구별하지않는 방법 s.toLowerCase()
+    // 3. string -> char[] -> List<Character>
+
+    boolean solution(String s) {
+        List<Character> charList = new ArrayList<>();
+        for (char ch : s.toCharArray()) {
+            charList.add(ch);
+        }
+        long pCount = charList.stream().filter(character -> "p".equalsIgnoreCase(character.toString())).count();
+        long yCount = charList.stream().filter(character -> "y".equalsIgnoreCase(character.toString())).count();
+
+        return pCount == yCount;
+    }
+}
