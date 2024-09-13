05d7fe7902e7b4668afc5c3190406184eebf1ee7
saysuhyun <saysuhyun@gmail.com>
2024-09-14 00:02:48 +0900

    feat: 문자열을 정수로 바꾸기

---
+++ suhyun/2week/문자열을_정수로_바꾸기.java
@@ -0,0 +1,9 @@
+public class 문자열을_정수로_바꾸기 {
+    public static void main(String[] args) {
+        soulution("-1234");
+    }
+
+    public static int soulution(String s) {
+        return Integer.parseInt(s);
+    }
+}
