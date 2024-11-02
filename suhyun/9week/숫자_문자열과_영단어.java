fadbc7f19d476ce43c65b7c84ae1c0630ab7f934
saysuhyun <saysuhyun@gmail.com>
2024-11-02 09:42:14 +0900

    feat: 숫자 문자열과 영단어

---
+++ suhyun/9week/숫자_문자열과_영단어.java
@@ -0,0 +1,11 @@
+public class 숫자_문자열과_영단어 {
+    public int solution(String s) {
+        String[] words = {"zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"};
+
+        for (int i = 0; i < words.length; i++) {
+            s = s.replaceAll(words[i], Integer.toString(i));
+        }
+
+        return Integer.parseInt(s);
+    }
+}
