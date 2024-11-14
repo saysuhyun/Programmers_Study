5c8e90310216d36ac21e7e4059a8834d456d9094
rasung-ki <devhesh@naver.com>
2024-11-14 20:12:42 +0900

    feat: 지폐 접기

---
+++ rasung/11week/지폐_접기.java
@@ -0,0 +1,32 @@
+class Solution {
+    public int solution(int[] wallet, int[] bill) {
+        int answer = 0;
+
+        while (true) {
+            if (bill[0] <= wallet[0] && bill[1] <= wallet[1]) {
+                break;
+            }
+
+            if (bill[0] > bill[1]) {
+                bill[0] /= 2;
+            } else {
+                bill[1] /= 2;
+            }
+            answer++;
+
+            if (bill[0] <= wallet[0] && bill[1] <= wallet[1]) {
+                break;
+            }
+
+            int swap = bill[0];
+            bill[0] = bill[1];
+            bill[1] = swap;
+
+            if (bill[0] <= wallet[0] && bill[1] <= wallet[1]) {
+                break;
+            }
+        }
+
+        return answer;
+    }
+}
